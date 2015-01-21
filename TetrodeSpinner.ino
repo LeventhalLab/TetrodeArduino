d6fd28808632eea01a1b6382a755e39bc104ef70
Matt <matt@primestudiosllc.com>
2015-01-21 16:50:41 -0500

    spinner file

---
+++ TetrodeSpinner.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+
+// Connect a stepper motor with 200 steps per revolution (1.8 degree)
+// to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
+
+boolean waiting = true;
+const int switch1 = 12;
+
+void setup() {
+  pinMode(switch1, INPUT);
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  myMotor->setSpeed(750);  // 10 rpm   
+}
+
+void loop() {
+  if(digitalRead(switch1) == HIGH) {
+    myMotor->step(200*1, FORWARD, DOUBLE); 
+    myMotor->step(200*1, BACKWARD, DOUBLE);
+  }
+}
